771d97c11247b03bd3b0378e46d472fe8b42d9be
anita-brown <efeaniita@gmail.com>
2023-01-02 23:52:36 +0100

    refix:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className='flex justify-between pt-8'>
           <h1 className='text-white font-bold'>APP</h1>
           <button className='flex items-center justify-center gap-2 rounded-full bg-btnColor px-4'>
-              <img width='25' src={profile} alt="profile" />
+              <img width='28' src={profile} alt="profile" />
               <p className='text-[16px]'>Logout</p>
           </button>
     </header>
